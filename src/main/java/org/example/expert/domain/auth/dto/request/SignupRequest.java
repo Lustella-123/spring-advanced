24a8c91df4625b50fc0a5969f3739141b7c7ddb7
Lustella-123 <tella4164@gmail.com>
2025-01-02 09:12:16 +0900

    Lv 3-1: Early Return

--- src/main/java/org/example/expert/domain/auth/dto/request/SignupRequest.java
+++ src/main/java/org/example/expert/domain/auth/dto/request/SignupRequest.java
@@ -2,6 +2,8 @@ package org.example.expert.domain.auth.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,9 +13,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class SignupRequest {
 
-    @NotBlank @Email
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @Email(message = "이메일 형식이 유효하지 않습니다.")
     private String email;
-    @NotBlank
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    @Size(min = 8)
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\d).+$")
     private String password;
     @NotBlank
     private String userRole;
